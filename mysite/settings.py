06dbddc560a27d7ea31590598b6d4f83aa6992df
guhe78 <131249647+guhe78@users.noreply.github.com>
2024-07-06 16:45:41 +0200

    Update settings.py
    in settings.py debug=false

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ew2+kl)iwvc2k5k15m)qh_l2@#r-ji-0*97)k%ola6b3k8i=)v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '-pythonanywhere.com']
 
